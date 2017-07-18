a14ddae563a41a2312bf39e143c28243ccbb1812
André Martins <andremartinsdsilva@gmail.com>
2017-07-17 23:42:05 -0300

    entendendo arrow parcialmente no es6

---
+++ es6/arrow.js
@@ -0,0 +1,11 @@
+//vamos entender o Arow
+let valores = ["12", "13", "23"];
+
+//fica bem legível esse código (dizemso valores recebe algo mais valores mais algo)
+let precosEmReais = valores.map(valores=>"R$" + valores + ",00");
+console.log(precosEmReais);
+
+//podemos fazer um filter e após isso aplicar um mapp
+let acimaDeVinte = valores.filter(valores=> valores > 20).map(valores=>"R$" + valores + ",00");
+console.log(acimaDeVinte);
+
