0d58f48947549983a2f03355c49fc6fe99c75114
André Martins <andremartinsdsilva@gmail.com>
2019-10-23 14:20:46 -0300

    exemplo básico de hoisting

---
+++ 01_instroducao/01_iniciando_var_e_let/01.1/hoisting.js
@@ -0,0 +1,9 @@
+/*
+    Não ha muita utilidade, basicamente o Hoisting do javascript joga a declaração
+    para a parte superior do objeto window
+*/
+console.log('a = ', a)
+
+var a = "joguei a declaração var para antes do console.log"
+
+console.log(a)
