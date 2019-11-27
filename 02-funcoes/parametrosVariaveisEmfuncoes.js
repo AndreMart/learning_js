111c4b7cc7a85c95d2fcaadbb885e028a18f9385
André Martins <andremartinsdsilva@gmail.com>
2019-11-27 14:01:33 -0300

    funções com parametros variáveis

---
+++ 02-funcoes/parametrosVariaveisEmfuncoes.js
@@ -0,0 +1,20 @@
+// Podemos passar para uma funçao parâmetros variáveis e recuperar os mesmos
+// por meio dos arguments gerados, vamos ver
+function soma() {
+    let soma = 0
+    for (const i in arguments) {
+        soma+=arguments[i]
+    }
+    return soma
+}
+//não passando parametro nenhum
+console.log(soma())
+
+// concatenando strings
+console.log(soma(" A ", "B ", " C"))
+
+//somando valores
+console.log(soma(1,2,3,4,5,6,4.5))
+
+// fazendo gambis
+console.log(soma(1, " uai"))
