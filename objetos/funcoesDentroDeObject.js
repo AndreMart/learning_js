663b7c3aee1c3fe7f83519c70c547eba8014e2c2
André Martins <andremartinsdsilva@gmail.com>
2019-12-29 16:24:20 -0300

    revisões sobre objetos

---
+++ objetos/funcoesDentroDeObject.js
@@ -0,0 +1,28 @@
+const pessoa = {
+    nome: "Mauro",
+    idade: 25,
+}
+// metodos interessantes 
+console.log(Object.keys(pessoa))
+console.log(Object.values(pessoa))
+console.log(Object.entries(pessoa))
+Object.entries(pessoa).forEach(([chave, valor]) => {
+    console.log(chave, valor)
+})
+//definindo uma propriedade
+Object.defineProperty(pessoa, "peso",{
+    // ela pode ser listada?
+    enumerable:true,
+    // ela pode ser reescrita?
+    writable:false,
+    // qual valor dela?
+    value:145
+})
+console.log(pessoa.peso)
+
+//função Object.assing é uma espécia de concatenação
+const dest = {a:1}
+const o1 = {b:2}
+const o2 = {c:3, a:4}
+const obj = Object.assign(dest, o1, o2)
+console.log(obj)
---
+++ objetos/notacaoLiteral.js
@@ -0,0 +1,8 @@
+const a = 1
+const b = 2
+const c = 3
+//antigamente
+const obj = {a:a, b:b, c:c}
+//agora
+const obj1 = {a, b, c}
+console.log(obj,obj1)
