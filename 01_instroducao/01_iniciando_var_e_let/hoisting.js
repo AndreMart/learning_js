4bd404b897d566e8f01768d0d46ba124eb76c828
André Martins <andremartinsdsilva@gmail.com>
2020-09-15 21:14:27 -0300

    hoisting

--- 01_instroducao/01_iniciando_var_e_let/hoisting.js
+++ 01_instroducao/01_iniciando_var_e_let/hoisting.js
@@ -2,8 +2,8 @@
     Não ha muita utilidade, basicamente o Hoisting do javascript joga a declaração
     para a parte superior do objeto window
 */
-console.log('a = ', a)
+console.log("a = ", a);
 
-var a = "joguei a declaração var para antes do console.log"
+var a = "joguei a declaração var para antes do console.log";
 
-console.log(a)
+console.log(a);
