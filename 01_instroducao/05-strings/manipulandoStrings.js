11d019c3b7ea9ae85a0bae52a45238920ff1609a
André Martins <andremartinsdsilva@gmail.com>
2021-02-22 16:08:20 -0300

    manipulando string revisao

--- 01_instroducao/05-strings/manipulandoStrings.js
+++ 01_instroducao/05-strings/manipulandoStrings.js
@@ -7,18 +7,18 @@ console.log(s.length);
 console.log(s.indexOf("coming"));
 
 //Pegando a primeira posição de um caractere
-console.log(s.lastIndexOf("winter"));
+console.log(s.lastIndexOf("the"));
 
 //cortando uma palavra entre intervalos
-console.log(s.slice(4,12));
+console.log(s.slice(4, 12));
 
 //cortando a partir de um indice (o parametro 6 é a partir do 4)
-console.log(s.substr(4,6));
+console.log(s.substr(4, 6));
 
-console.log(s.substring(1,5))
+console.log(s.substring(1, 5));
 
 //replace
-console.log(s.replace("winter","summer"));
+console.log(s.replace("winter", "summer"));
 
 //upperCase
 console.log(s.toUpperCase());
@@ -34,4 +34,8 @@ var novaString = "Ned,Jon,Robb,Sansa,Bryan";
 //separar por determinado caractére
 console.log(novaString.split(","));
 
-
+let novaString2 = "André";
+const n = [...novaString2];
+console.log(n);
+const n2 = n.map((n, i) => `${n.toLocaleUpperCase()}${i}`);
+console.log(n2);
